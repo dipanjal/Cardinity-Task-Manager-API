a0eda040b253b331eb720bf5cdc22b81ec7e7c44
dipanjal <dipanjal.maitra@bs-23.net>
2021-02-09 02:30:06 +0600

    Password Encoder Added

--- src/main/java/rest/api/cardinity/taskmanager/config/SecurityConfiguration.java
+++ src/main/java/rest/api/cardinity/taskmanager/config/SecurityConfiguration.java
@@ -10,6 +10,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -55,7 +56,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Bean
     public PasswordEncoder encoder() {
-        return NoOpPasswordEncoder.getInstance();
+        return new BCryptPasswordEncoder(11);
     }
 
     private String[] getAllowedUrls(){
